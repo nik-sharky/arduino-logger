d845f2075e9afae2bc479b7e0671b84a3b9acf6a
sharky <nik.sharky@gmail.com>
2017-10-10 11:57:33 +0300

    Fix log_printf for proper work on esp8266

--- src/log.h
+++ src/log.h
@@ -6,19 +6,32 @@
 #define log_println(...) LOG.println(__VA_ARGS__)
 #define log_printf(...) _log_printf(__VA_ARGS__)
 
-const size_t _log_printf(const char *szFormat, ...) {
-  va_list argptr;
-  va_start(argptr, szFormat);
-  char *szBuffer = 0;
-  const size_t nBufferLength = vsnprintf(szBuffer, 0, szFormat, argptr) + 1;
-  if (nBufferLength == 1) return 0;
-  szBuffer = (char *) malloc(nBufferLength);
-  if (! szBuffer) return - nBufferLength;
-  vsnprintf(szBuffer, nBufferLength, szFormat, argptr);
-  log_print(szBuffer);
-  free(szBuffer);
-  return nBufferLength - 1;
+const size_t _log_printf(const char *format, ...) {
+    va_list arg;
+    va_start(arg, format);
+    char temp[64];
+    char* buffer = temp;
+    size_t len = vsnprintf(temp, sizeof(temp), format, arg);
+    va_end(arg);
+    if (len > sizeof(temp) - 1) {
+        buffer = (char *) malloc(len+1);
+        //buffer = new char[len + 1];
+        if (!buffer) {
+            return 0;
+        }
+        va_start(arg, format);
+        vsnprintf(buffer, len + 1, format, arg);
+        va_end(arg);
+    }
+    //len = write((const uint8_t*) buffer, len);
+    log_print(buffer);
+    if (buffer != temp) {
+        free(buffer);
+        //delete[] buffer;
+    }
+    return len;
 }
+
 #else
 
 #define log_print(...)((void) 0)
